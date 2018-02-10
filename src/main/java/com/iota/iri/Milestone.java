a3f5eab7f383d812399f94d3def101fab8bdb4f5
cagec7 <cagecq@163.com>
2018-02-10 14:55:31 +0800

    disable milestone check.

--- src/main/java/com/iota/iri/Milestone.java
+++ src/main/java/com/iota/iri/Milestone.java
@@ -203,6 +203,7 @@ public class Milestone {
                                         ISS.NUMBER_OF_FRAGMENT_CHUNKS),
                                 signatureFragmentTrits)),
                                 transactionViewModel2.trits(), 0, index, NUMBER_OF_KEYS_IN_A_MILESTONE);
+                        
                         //if (testnet || (new Hash(merkleRoot)).equals(coordinator)) {
                             new MilestoneViewModel(index, transactionViewModel.getHash()).store(tangle);
                             return VALID;
